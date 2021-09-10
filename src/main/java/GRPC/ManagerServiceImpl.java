3cc85fcdb06872ba5c01cf8b7cfcbff51bd42edc
Federico Piscitelli <>
2021-09-10 08:23:15 +0200

    Fix comments

--- src/main/java/GRPC/ManagerServiceImpl.java
+++ src/main/java/GRPC/ManagerServiceImpl.java
@@ -15,16 +15,17 @@ public class ManagerServiceImpl extends ManagerGrpc.ManagerImplBase {
         this.d = d;
     }
 
+
     @Override
     public void welcome(Welcome.WelcomeMessage request, StreamObserver<Welcome.WelcomeResponse> responseObserver) {
-        //adding the drone to my list is equivalent to adding the drone to my topology of the network
 
         List<Drone> myDronesList = d.getDronesList();
-        int originalSize = myDronesList.size();
+        int originalSize = myDronesList.size(); //original size of the list used to avoid infinite loop
 
         for(int i=0; i<originalSize; i++){
             if(request.getId()<Integer.parseInt(myDronesList.get(i).getId())){
                 Drone droneToInsert = new Drone(String.valueOf(request.getId()), request.getIp(), request.getPort());
+                //adding the drone to my list is equivalent to adding the drone to my topology of the network
                 myDronesList.add(i,droneToInsert);
             }
         }
@@ -39,5 +40,6 @@ public class ManagerServiceImpl extends ManagerGrpc.ManagerImplBase {
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
+
 
 }
